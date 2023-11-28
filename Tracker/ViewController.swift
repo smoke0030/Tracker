6ef6741a3e0729c96e6b70d7203d5f3cfc401f22
smoke0030 <ft.priest@gmail.com>
2023-11-28 16:22:04 +0300

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Сергей on 28.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
